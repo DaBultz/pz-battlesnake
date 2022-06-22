d695b027d6e3531c0a9bd8cae2f047cb89ffd593
Bultz <27208227+DaBultz@users.noreply.github.com>
2022-06-22 18:32:21 +0200

    feat(env): start BaseEnv

---
+++ pz_battlesnake/base_env.py
@@ -0,0 +1,10 @@
+from pettingzoo import ParallelEnv
+
+
+class BaseEnv(ParallelEnv):
+    """
+    The BaseEnv implements all of the shared functionality of all environments using the parallel epi.
+    this means that it steps all agents at once rather than one at a time.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
